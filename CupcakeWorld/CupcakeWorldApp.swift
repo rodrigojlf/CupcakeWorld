6da6051c2ef488f31d79ed06572a42af14fe8949
rodrigojlf <rjorgefilho@gmail.com>
2026-03-13 20:19:13 -0300

    Initial Commit

---
+++ CupcakeWorld/CupcakeWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeWorldApp.swift
+//  CupcakeWorld
+//
+//  Created by Rodrigo Lima on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
